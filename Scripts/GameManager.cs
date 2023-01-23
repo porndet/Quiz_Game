fb1edb97709c758662b35dfa2630639e14e51acb
Porndet Pipitsuntonsan <likeink2@hotmail.com>
2023-01-23 22:06:49 +0900

    new commit

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -23,10 +23,19 @@ public class GameManager : MonoBehaviour
     private string imagePath{
         get; set;
     }
+
+    private List<Sprite> Image_Mistake{
+        get; set;
+    } = new List<Sprite>();
+    private List<string> Image_PathMistake{
+        get; set;
+    } = new List<string>();
+
     public int length_arr = 4;
 
     void Start(){
         StartCoroutine(GetRequest("http://127.0.0.1/dashboard/website_project/FetchDataDB/QuizGame.php", SceneManager.GetActiveScene().buildIndex + 1));
+        StartCoroutine(GetRequest_Mistake("http://127.0.0.1/dashboard/website_project/FetchDataDB/QuizGame.php", SceneManager.GetActiveScene().buildIndex + 1));
     }
 
     IEnumerator GetRequest(string URL, int Pathid)
@@ -57,6 +66,40 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    IEnumerator GetRequest_Mistake(string URL, int Pathid)
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("Path_Mistake", Pathid);
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(URL, form))
+        {
+            yield return webRequest.SendWebRequest();
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    JSONNode jsonNode = JSON.Parse(webRequest.downloadHandler.text);
+                    foreach(JSONNode n in jsonNode)
+                        Image_PathMistake.Add(n["filename"]);
+
+                    foreach(string s in Image_PathMistake){
+                        Debug.Log(s);
+                        LoadImage(s, Pathid);
+                    }
+
+                    break;
+            }
+        }
+    }
+
     private void LoadImage(string filename, int id){
         string path = "";
         if(id == 1){
@@ -74,18 +117,26 @@ public class GameManager : MonoBehaviour
         Texture2D loadTexture = new Texture2D(1, 1);
         loadTexture.LoadImage(bytes);
 
-        // Texture2D targetTexture = ResizeTexture(loadTexture, 65, 45);
-
         Sprite sprite = Sprite.Create(loadTexture, new Rect(0, 0, loadTexture.width, loadTexture.height), new Vector2(0.5f, 0.5f));
-        ImageUse = sprite;
-        // OpenPathFace.transform.Find(folder).transform.GetComponent<SpriteRenderer>().sprite = sprite;
-
-        // dataPathFace.Add(sprite);     
+        ImageUse = sprite;  
     }
 
-    // static Texture2D ResizeTexture(Texture2D srcTexture, int newWidth, int newHeight) {
-    //     var resizedTexture = new Texture2D(newWidth, newHeight);
-    //     Graphics.ConvertTexture(srcTexture, resizedTexture);
-    //     return resizedTexture;
-    // }
+        private void LoadImage_Mistake(string filename, int id){
+        string path = "";
+        if(id == 1 || id == 2){
+            path = @"C:\Users\INK\Desktop\Path_Face\" + filename;
+        }else if(id == 3){
+            path = @"C:\Users\INK\Desktop\Path_Face\" + filename;
+        }else if(id == 4){
+            path = @"C:\Users\INK\Desktop\Path_Face\" + filename;
+        }
+
+        byte[] bytes = File.ReadAllBytes(path);
+
+        Texture2D loadTexture = new Texture2D(1, 1);
+        loadTexture.LoadImage(bytes);
+
+        Sprite sprite = Sprite.Create(loadTexture, new Rect(0, 0, loadTexture.width, loadTexture.height), new Vector2(0.5f, 0.5f));
+        Image_Mistake.Add(sprite);
+    }
 }
